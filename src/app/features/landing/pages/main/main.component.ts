6e475e6d2ca0176d8767c0c47f70f8ec9e309fb5
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2025-06-27 17:07:37 +0300

    Update design + integrate new slider

--- src/app/features/landing/pages/main/main.component.ts
+++ src/app/features/landing/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
 import { SwiperComponent } from '../../components/swiper/swiper.component';
@@ -12,6 +12,14 @@ import { SwiperComponent } from '../../components/swiper/swiper.component';
 })
 export class MainComponent implements OnInit {
     private title = inject(Title);
+    @ViewChild('heroImages1') heroImages1!: ElementRef<HTMLDivElement>;
+    @ViewChild('heroImages2') heroImages2!: ElementRef<HTMLDivElement>;
+    public mouseEnterBlock1: boolean = false;
+    public mouseEnterBlock2: boolean = false;
+
+    public speed = 0.5;
+    private offset1 = 0;
+    private offset2 = 0;
 
     public slides = [
         { imageUrl: 'assets/voices/6.jpg' },
@@ -22,7 +30,71 @@ export class MainComponent implements OnInit {
         { imageUrl: 'assets/voices/19.jpg' },
     ];
 
+    public images1 = [
+        'assets/img/test/3.png',
+        'assets/img/test/2.png',
+        'assets/img/test/1.png',
+        'assets/img/test/4.png',
+        'assets/img/test/5.png',
+        'assets/img/test/6.png'
+    ];
+
+    public images2 = [
+        'assets/img/test/4.png',
+        'assets/img/test/5.png',
+        'assets/img/test/6.png',
+        'assets/img/test/1.png',
+        'assets/img/test/2.png',
+        'assets/img/test/3.png'
+    ];
+
     public ngOnInit(): void {
+        this.images1 = [...this.images1, ...this.images1];
+        this.images2 = [...this.images2, ...this.images2];
         this.title.setTitle('Wondrvoices');
+    }
+
+    public ngAfterViewInit(): void {
+        setTimeout(() => {
+            this.startAnimation();
+        }, 100);
+    }
+
+    private startAnimation(): void {
+        const block1 = this.heroImages1.nativeElement;
+        const block2 = this.heroImages2.nativeElement;
+
+        const step = () => {
+            
+            if (!this.mouseEnterBlock1) {
+                this.offset1 -= this.speed;
+            }
+
+            if (!this.mouseEnterBlock2) {
+                this.offset2 -= this.speed;
+            }
+
+            this.moveAndLoop(block1, true);
+            this.moveAndLoop(block2, false);
+
+            requestAnimationFrame(step);
+        };
+
+        requestAnimationFrame(step);
+    }
+
+    private moveAndLoop(block: HTMLElement, isUp: boolean): void {
+        if (isUp) {
+            block.style.transform = `translateY(${this.offset1}px)`;
+            const blockHeight = block.scrollHeight / 2;
+            if (Math.abs(this.offset1) >= blockHeight + 8) {
+                this.offset1 = 0;
+            }
+        } else {
+            block.style.transform = `translateY(${-block.scrollHeight / 2 - this.offset2}px)`;
+            if (Math.abs(this.offset2) >= block.scrollHeight / 2 + 8) {
+                this.offset2 = 0;
+            }
+        }
     }
 }
