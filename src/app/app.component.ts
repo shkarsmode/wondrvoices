0ff551b1a127cb694a1a7b83486eb70a3e61ba04
Shkarupa Daniil <zshkarrr@gmail.com>
2025-08-05 17:16:28 +0300

    Add service to scroll to top after every route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { ScrollToService } from './shared/services/scroll-to.service';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [RouterOutlet],
     template: '<router-outlet></router-outlet>',
+    providers: [ScrollToService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'wondrvoices';
+    private readonly scrollToService: ScrollToService = inject(ScrollToService);
+    private readonly router: Router = inject(Router);
+
+    public ngOnInit(): void {
+        this.listenRoutesTransition();
+    }
+
+    private async listenRoutesTransition(): Promise<void> {
+        // @ts-ignore
+        this.router.events.subscribe(async (event: any) => {
+            if (event instanceof NavigationEnd) {
+                // await new Promise(resolve => setTimeout(resolve, 50));
+                this.scrollToService.scrollToTop();
+            }
+        });
+    }
 }
