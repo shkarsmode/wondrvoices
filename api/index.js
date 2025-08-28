e3a80cc6229e2987cb089e88f63317c4975da8b1
Shkarupa Daniil <zshkarrr@gmail.com>
2025-08-28 13:23:08 +0300

    Update

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
-const { app } = import("../dist/wondrvoices/server/main.js");
-
-export default app();
+export default async (req, res) => {
+  const { app } = await import("../dist/wondrvoices/server/main.js");
+  return app(req, res);
+};
