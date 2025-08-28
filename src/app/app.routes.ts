41610991f3c6e43c9403cb01f330f7dbf8246d59
Shkarupa Daniil <zshkarrr@gmail.com>
2025-08-28 11:45:44 +0300

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -54,7 +54,7 @@ export const routes: Routes = [
                     .then(m => m.TermsComponent)
             },
             {
-                path: 'privacy/privacy',
+                path: 'privacy',
                 loadComponent: () => import('./features/landing/pages/privacy/privacy.component')
                     .then(m => m.PrivacyComponent)
             }
