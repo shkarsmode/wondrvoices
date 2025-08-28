d90ceba296ae56394ccf4bf91b0c7fc3d2f1741c
Shkarupa Daniil <zshkarrr@gmail.com>
2025-08-28 13:00:44 +0300

    Update

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
+import("../dist/wondrvoices/server/server.mjs")
+  .then(module => module.app)
+  .catch(error => {
+    console.error("Failed to load server module:", error);
+    throw error;
+  });
 export default async (req, res) => {
   const { app } = await import("../dist/wondrvoices/server/server.mjs");
   return app(req, res);
