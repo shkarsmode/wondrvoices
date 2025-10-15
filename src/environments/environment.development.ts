0924c8701801991e1c483b64ddd465d1d5b94cab
Shkarupa Daniil <zshkarrr@gmail.com>
2025-10-15 14:11:26 +0300

    Update dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,5 +5,5 @@ export const environment = {
     geoPathAPI: 'https://api.geoapify.com/v1/geocode/autocomplete',
     geoApiKey: '3b23dc57405b446fbbf29846a2d2b6dd',
     locationIqToken: 'pk.0e75ab005ceb08e738472e1ae6f534ca',
-    geniusscansdkToken: '533c5007525500020157065239095701525554560107070c55185a0145575b1d04460107165446473a03060451520501505252'
+    geniusscansdkToken: '533c5007525500020151035139465a0d0011440b5d55574a4c555b0b68090706510450540f0200'
 };
