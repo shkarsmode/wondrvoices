58cd2bb7aedc4c292008a60b818f735f14393ae9
Shkarupa Daniil <zshkarrr@gmail.com>
2025-10-28 13:27:48 +0200

    Update default filter

--- src/app/features/landing/pages/form/form.component.ts
+++ src/app/features/landing/pages/form/form.component.ts
@@ -634,6 +634,7 @@ export class FormComponent {
 
             const cfg: any = {
                 multiPage: false,
+                defaultFilter: 'photo',
                 multiPageFormat: 'none',
                 jpegQuality: 75,
             };
